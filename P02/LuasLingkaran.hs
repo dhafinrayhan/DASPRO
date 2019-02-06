763f0726a063d2906546b37533870d0109e34f1b
Dhafin Rayhan <bytecone@gmail.com>
2019-02-06 12:06:04 +0700

    Create LuasLingkaran.hs

---
+++ P02/LuasLingkaran.hs
@@ -0,0 +1,10 @@
+module LuasLingkaran where
+-- LuasLingkaran - luasLingakaran(r)
+-- DEFINISI DAN SPESIFIKASI
+luasLingkaran :: Float -> Float
+        -- luasLingkaran(r) menghitung luas lingkaran
+        -- dari jari-jari r, sebuah bilangan real
+-- REALISASI
+luasLingkaran r = 3.1415 * r * r
+-- CONTOH APLIKASI
+-- luasLingkaran 2
