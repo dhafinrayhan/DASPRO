21949ccb41cd257d5dad7e559da3e1a9c480b554
Dhafin Rayhan <bytecone@gmail.com>
2019-03-08 18:55:52 +0700

    Update 2.hs

--- UTS/2.hs
+++ UTS/2.hs
@@ -1,20 +1,20 @@
 -- 2a
 -- DEFINISI DAN SPESIFIKASI
-   isTahunKabisat :: Int -> Bool
-       -- menerima masukan integer x
-       -- lalu mengembalikan true bila x merupakan tahun kabisat
+isTahunKabisat :: Int -> Bool
+    -- menerima masukan integer x
+    -- lalu mengembalikan true bila x merupakan tahun kabisat
 -- REALISASI
-   isTahunKabisat x = if ((mod x 4)==0)&&((mod x 100)/=0)||((mod x 400)==0) then True
-                      else False
+isTahunKabisat x = if ((mod x 4)==0)&&((mod x 100)/=0)||((mod x 400)==0) then True
+                   else False
 -- CONTOH APLIKASI
 -- isTahunKabisat 2100
 -- False
 
 -- 2b
 -- DEFINISI DAN SPESIFIKASI
-   hitungTahunKabisat :: Int -> Int -> Int
-       -- menerima 2 buah integer a dan b
-       -- lalu mengembalikan banyaknya tahun kabisat dari a sampai b
+hitungTahunKabisat :: Int -> Int -> Int
+    -- menerima 2 buah integer a dan b
+    -- lalu mengembalikan banyaknya tahun kabisat dari a sampai b
 -- REALISASI
-   hitungTahunKabisat a b = if a > b then 0 -- Basis
-                            else (if (isTahunKabisat a) then 1 else 0) + (hitungTahunKabisat (a+1) b) -- Rekurens
+hitungTahunKabisat a b = if a > b then 0 -- Basis
+                         else (if (isTahunKabisat a) then 1 else 0) + (hitungTahunKabisat (a+1) b) -- Rekurens
