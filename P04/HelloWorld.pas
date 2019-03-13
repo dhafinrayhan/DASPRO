05b0ae79461c8610654c42b5ecf7669557caa192
Dhafin Rayhan <bytecone@gmail.com>
2019-03-13 12:07:00 +0700

    Add files via upload

---
+++ P04/HelloWorld.pas
@@ -0,0 +1,9 @@
+Program HelloWorld;
+{ Mencetak Hello, World! ke layar }
+
+{ KAMUS }
+
+{ ALGORITMA }
+begin
+    writeln('Hello, World!');
+end.
---
+++ P04/Jumlah3Bilangan.pas
@@ -0,0 +1,12 @@
+Program Jumlah3Bilangan;
+{ Menerima masukan 3 buah bilangan dan menuliskan penjumlahan ketiga bilangan ke layar }
+{ KAMUS }
+var
+    A, B, C : integer;
+{ ALGORITMA }
+begin
+    { Input A, B, C }
+    readln(A, B, C);
+    { Output hasil penjumlahan }
+    writeln(A+B+C);
+end.
